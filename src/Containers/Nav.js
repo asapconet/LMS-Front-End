936bd41b909d4d4b3e37020d66bf1a66302bae66
0tuedon <tuedonmofe@gmail.com>
2021-11-27 08:21:08 +0100

    Created all Neccessary Containers Including Nav and Footer in the Containers Folder

--- src/Containers/Nav.js
+++ src/Containers/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = () => {
+const Nav = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Button = () => {
     )
 }
 
-export default Button
+export default Nav

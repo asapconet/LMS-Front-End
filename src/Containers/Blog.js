f7dfaa9febbf3ae764e4849d1d3c3454f8343782
asapconet <aaronrickymeek@gmail.com>
2021-12-03 18:09:48 +0100

    done with single post view

--- src/Containers/Blog.js
+++ src/Containers/Blog.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Button1 from "../Components/button";
+import SinglePost from "./SinglePost";
 
 const Blog = () => {
   return (
-    <div className='flex h-screen justify-center items-center text-white text-center'>
+    <>
+    <div className="flex flex-col h-screen bg-black justify-center items-center text-white text-center">
       <div>
-        <h1 className='capitalize font-bold'>learn all your computer science courses with courseLab</h1>
+        <h1 className="capitalize font-bold">
+          learn all your computer science courses with courseLab
+        </h1>
         <p className="py-7">
           something something something about nothing and nothing became
           somthing we never knew <br />
@@ -13,8 +17,12 @@ const Blog = () => {
           somthing we never knew something something something about nothing
         </p>
         <Button1>Start Your Courses</Button1>
-      </div>
     </div>
+      </div>
+      <div className=" singlepost px-40">
+        <SinglePost />
+      </div>
+      </>
   );
 };
 
---
+++ src/data.js
@@ -0,0 +1,35 @@
+import postimg from "./assets/postimg.JPG";
+
+const post = [
+  {
+    id: "0",
+    image: postimg,
+    desc: 'jabajantis common jabajanti abajanti common jabajantis common jabajanti common{" "}',
+    name: "Piper",
+  },
+  {
+    id: "1",
+    image: postimg,
+    desc: 'jabajantis common jabajanti abajanti common jabajantis common jabajanti common{" "}',
+    name: "Janer",
+  },
+  {
+    id: "2",
+    image: postimg,
+    desc: 'jabajantis common jabajanti abajanti common jabajantis common jabajanti common{" "}',
+    name: "Masgue",
+  },
+  {
+    id: "3",
+    image: postimg,
+    desc: 'jabajantis common jabajanti abajanti common jabajantis common jabajanti common{" "}',
+    name: "Syns",
+  },
+  {
+    id: "3",
+    image: postimg,
+    desc: 'jabajantis common jabajanti abajanti common jabajantis common jabajanti common{" "}',
+    name: "Piper",
+  },
+];
+export default post;

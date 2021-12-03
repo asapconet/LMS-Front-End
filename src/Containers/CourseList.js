6c9bdc3619fc7e71f79ec8d25ec364d0def7173f
0tuedon <tuedonmofe@gmail.com>
2021-12-03 22:22:03 +0100

    Designed the Login page and Installed Axios

--- src/Containers/CourseList.js
+++ src/Containers/CourseList.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './CourseList.css'
+import CourseComponent from '../Components/CoursesComponent'
 const CourseList = () => {
     return (
-        <div>
-                    
+        <div className="courses-container">
+            <div className="available-courses">
+                <div className="header-text">
+                    <strong><h1>COURSES AVAILABLE IN OUR DATABASE</h1></strong>
+                </div>
+                <div className="course-section">
+                    <div className="border-b">
+                        <CourseComponent/>
+                        <CourseComponent/>
+                        <CourseComponent/>
+                        <CourseComponent/>
+                        <CourseComponent/>
+                        <CourseComponent/>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }

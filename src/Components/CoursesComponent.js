28cf0ceb457378b9eaaf4806827628476a217a92
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-12-16 07:07:23 +0100

    Feature: Implemented delete course

--- src/Components/CoursesComponent.js
+++ src/Components/CoursesComponent.js
@@ -1,58 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { FaDownload } from "react-icons/fa";
-import axios from "axios";
-import { GetMyCoursesURL } from "../API/BaseURL";
+import { Link, useNavigate } from "react-router-dom";
+import { client } from "../API/requests";
 
-const Courses = () => {
-  const [courses, setCourse] = useState([]);
-
-  const courseHandler = (e) => {
-    // e.preventDefault();
-
-    axios.get(GetMyCoursesURL).then((res) => {
-      let myCourse = res.data.results;
-      setCourse(myCourse);
-      console.log(myCourse);
-    });
+const Courses = ({ posted_by, level, uuid, created, last_updated, cover, title, content, description, slug }) => {
+  const navigate = useNavigate();
+  const handleDelete = () => {
+    console.log("delete");
+    client.defaults.headers.common["Authorization"] = `JWT ${localStorage.getItem("access")}`;
+    client.delete(`/resources/${uuid}/`, {
+      uuid: uuid
+    })
+      .then((res) => {
+        console.log(res);
+        navigate("/user/courses");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   };
-
-  useEffect(() => {
-    courseHandler();
-  }, []);
-
   return (
-    <>
-      {courses.map((data, uuid) => {
-        return (
-          <section key={uuid}>
-            <div className="course-item flex justify-between bg-black text-white rounded m-2">
-              <div className="font-bold text-lg p-2 m-1 uppercase">
-                <p>{data.title}</p>
-              </div>
-              <div className="">
-                <button className="course-button bg-gray-600 hover:bg-gray-700 text-white text-lg p-2 m-1 border rounded">
-                  Edit
-                </button>
-              </div>
-              <div>
-                <button className="course-button bg-gray-600 hover:bg-red-gray text-white text-lg p-2 my-1 border rounded">
-                  <p>Delete</p>
-                </button>
-              </div>
-              <div className="rounded-full p-1 h-9 text-sm m-1 font-bold bg-gray-600">
-                <a
-                  href="../assests/IMG.PNG"
-                  download="picture"
-                  className="flex items-center"
-                >
-                  <FaDownload />
-                </a>
-              </div>
-            </div>
-          </section>
-        );
-      })}
-    </>
+    <div className="course-item flex justify-between bg-black text-white rounded m-2">
+      <div className="font-bold text-lg p-2 m-1 uppercase">
+        <p>{title}</p>
+      </div>
+      <div className="">
+        <Link to={``} className="course-button bg-gray-600 hover:bg-gray-700 text-white text-lg p-2 m-1 border rounded">
+          View
+        </Link>
+      </div>
+      <div>
+        <button
+          onClick={handleDelete}
+          className="course-button bg-gray-600 hover:bg-red-gray text-white text-lg p-2 my-1 border rounded">
+          Delete
+        </button>
+      </div>
+      <div className="rounded-full p-1 h-9 text-sm m-1 font-bold bg-gray-600">
+        <a href={content} download='picture' className="flex items-center">
+            <FaDownload/>
+        </a>
+      </div>
+    </div>
   );
 };
 
--- src/Containers/CourseList.js
+++ src/Containers/CourseList.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button1 from "../Components/button";
 import { FaHome } from "react-icons/fa";
 import CourseComponent from "../Components/CoursesComponent";
 import { Link } from "react-router-dom";
+import { useLoadItem } from "../utils/useLoadItem";
+import { ResourceURL } from "../API/BaseURL";
+import { Loading } from "../Components/Loading";
+import AuthContext from "../Context/AuthContext";
 
 const CourseList = () => {
-
-  
+  const { user } = useContext(AuthContext)
+  const username = user.username
+  const { loading, state, refetch } = useLoadItem(`${ResourceURL}?user=${username}`);
   return (
     <div className="courses-container">
       <div className="available-courses">
@@ -20,14 +25,24 @@ const CourseList = () => {
         </div>
         <div className="course-section">
           <div className="border-b px-3">
-            <CourseComponent />
+            {state?.posts?.map((course) => (
+              <CourseComponent key={course.uuid} {...course} />
+            ))}
           </div>
           <Link to="/courses/create" className="flex justify-end my-5 px-5">
             <Button1 className="hover:shadow-lg animate-pulse">
               Upload a course
             </Button1>
           </Link>
-        </div>
+          </div>
+        {loading
+          ? <Loading />
+          : <>{state.next && <button
+            onClick={refetch}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+          Load More
+        </button>}</>}
       </div>
     </div>
   );

421afbaa2ec8cbc0e596b4fca9300e3e8848723e
asapconet <aaronrickymeek@gmail.com>
2022-01-24 23:31:27 +0100

    Bug: removed and refixed authcontext folder

--- src/Containers/Context/AuthContext.js
+++ src/Containers/Context/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { MeURL, VerifyLoginUrl } from "../API/BaseURL";
-import { client } from "../API/requests";
+import { MeURL, VerifyLoginUrl } from "../../API/BaseURL";
+import { client } from "../../API/requests";
 
 const AuthContext = React.createContext({
   isLoggedIn: "",
